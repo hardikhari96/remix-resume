7321576d0255f19cc9857c2f86a3decd38450055
Harikrushna <32766262+hardikhari96@users.noreply.github.com>
2023-01-22 14:26:31 +0530

    Simple resume

---
+++ app/components/header/footbar.tsx
@@ -0,0 +1,12 @@
+export function FootBar() {
+    
+    return (
+        <>
+        <div>
+            <div className="footer-1">
+                Thank You
+            </div>
+        </div>
+        </>
+    );
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,79 +1,23 @@
-import type { MetaFunction, LoaderFunction } from "remix";
-import { useLoaderData, json, Link } from "remix";
+import { json, LoaderFunction } from "remix";
 
-type IndexData = {
-  resources: Array<{ name: string; url: string }>;
-  demos: Array<{ name: string; to: string }>;
-};
 
-// Loaders provide data to components and are only ever called on the server, so
-// you can connect to a database or run any server side code you want right next
-// to the component that renders it.
-// https://remix.run/api/conventions#loader
 export let loader: LoaderFunction = () => {
-  let data: IndexData = {
-    resources: [
-
-    ],
-    demos: [
-
-    ]
-  };
-  // https://remix.run/api/remix#json
-  return json(data);
+  return json({});
 };
 
-// https://remix.run/api/conventions#meta
-export let meta: MetaFunction = () => {
-  return {
-    title: "Remix Starter",
-    description: "Welcome to remix!"
-  };
-};
-let imageStyle = {
-  height: "100px",
-  width: "auto"
-}
-// https://remix.run/guides/routing#index-routes
 export default function Index() {
-  let data = useLoaderData<IndexData>();
-
   return (
-
-    <nav className="bg-white shadow dark:bg-gray-800">
-      <div className="container px-6 py-4 mx-auto">
-        <div className="md:flex md:items-center md:justify-between">
-          <div className="flex items-center justify-between">
-            <div className="text-xl font-semibold text-gray-700">
-              <a className="text-2xl font-bold text-gray-800 transition-colors duration-200 transform dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300" href="#">HP</a>
-            </div>
-            <div className="flex md:hidden">
-              <button type="button" className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
-              </button>
-            </div>
-          </div>
-
-          <div className="flex-1 md:flex md:items-center md:justify-between">
-            <div className="flex flex-col -mx-4 md:flex-row md:items-center md:mx-8">
-              <a href="#" className="px-2 py-1 mx-2 mt-2 text-sm font-medium text-gray-700 transition-colors duration-200 transform rounded-md md:mt-0 dark:text-gray-200 hover:bg-gray-300 dark:hover:bg-gray-700">Github</a>
-            </div>
-
-            <div className="flex items-center mt-4 md:mt-0">
-
-              <button type="button" className="flex items-center focus:outline-none" aria-label="toggle profile dropdown">
-                <div className="w-8 h-8 overflow-hidden border-2 border-gray-400 rounded-full">
-                  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/HP_New_Logo_2D.svg/2048px-HP_New_Logo_2D.svg.png" className="object-cover w-full h-full" alt="avatar" />
-                </div>
-
-                <h3 className="mx-2 text-sm font-medium text-gray-700 dark:text-gray-200 md:hidden">Harikrushna Patel</h3>
-                <a className="block w-1/2 px-3 py-2 mx-1 text-sm font-medium leading-5 text-center text-white transition-colors duration-200 transform bg-gray-500 rounded-md hover:bg-blue-600 md:mx-2 md:w-auto" href="https://auth.haripatel.dev/login?r=www">Login</a>
-                <a className="block w-1/2 px-3 py-2 mx-1 text-sm font-medium leading-5 text-center text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-600 md:mx-0 md:w-auto" href="#">Join free</a>
-                <a className="block w-1/2 px-3 py-2 mx-1 text-sm font-medium leading-5 text-center text-white transition-colors duration-200 transform bg-gray-500 rounded-md hover:bg-blue-600 md:mx-2 md:w-auto" href="#">Logout</a>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
+    <>
+    {/* <img src="https://auth.haripatel.dev/logo.png"></img> */}
+      <h1 className="text-center mt-24">
+        Hello World! I am  <a target={"_blank"} className="bg-black hover:bg-white  focus:outline-none focus:ring focus:ring-black-300 text-white hover:text-black" href="https://github.com/hardikhari96">
+          Harikrushna Patel.
+        </a>
+      </h1>
+      <h1 className="text-center mt-12">
+        Visit My   <a target={"_blank"} className="bg-black hover:bg-white  focus:outline-none focus:ring focus:ring-black-300 text-white hover:text-black" href="https://library.haripatel.dev">
+        Library.
+        </a>
+      </h1>
+    </>)
 }
